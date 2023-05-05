6a85b9732c56920ef8914001d71bbb175339ca2e
moder <moder@LAPTOP-B1UKR6QM>
2023-05-05 15:02:04 +0300

    Add project files.

---
+++ MvvmApp/MvvmApp/Views/FriendPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MvvmApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MvvmApp.Views
+{
+    public partial class FriendPage : ContentPage
+    {
+        public FriendViewModel ViewModel { get; private set; }
+        public FriendPage(FriendViewModel vm)
+        {
+            InitializeComponent();
+            ViewModel = vm;
+            BindingContext = ViewModel;
+        }
+    }
+}
---
+++ MvvmApp/MvvmApp/Views/FriendsListPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MvvmApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MvvmApp.Views
+{
+    public partial class FriendsListPage : ContentPage
+    {
+        public FriendsListPage()
+        {
+            InitializeComponent();
+            BindingContext = new FriendsListViewModel() { Navigation = Navigation };
+        }
+    }
+}
